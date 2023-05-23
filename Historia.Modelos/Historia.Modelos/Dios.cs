5e26c5978375a956152c72a66511b040064015dc
Henry Ramírez <luishenryramirezmontalvo746@gmail.com>
2023-05-23 18:20:37 -0500

    Proyecto de Modelos Terminado

---
+++ Historia.Modelos/Historia.Modelos/Dios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Historia.Modelos
+{
+    public class Dios
+    {
+        public int Id { get; set; }
+        public string ?NombreDivino { get; set; }
+        public string NombrePopular { get; set; }
+        public string ?Elemento { get; set; }
+        public string ?Descripcion { get; set; }
+
+        //Atributos de navegacion
+        public int MitologiaId { get; set; }
+
+        //Relaciones
+        public Mitologia? Mitologia { get; set; }
+
+    }
+}
---
+++ Historia.Modelos/Historia.Modelos/Mitologia.cs
@@ -0,0 +1,13 @@
+﻿namespace Historia.Modelos
+{
+    public class Mitologia
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string PaisOrigen { get; set; }
+        public DateTime FechaOrigen { get; set; }
+
+        //Relacion
+        public List<Dios>? Dioses { get; set; }
+    }
+}
